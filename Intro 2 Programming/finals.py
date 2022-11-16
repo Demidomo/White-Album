729dbb3159a13cb19bd2413f8894ce7ae5414212
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2022-11-15 20:19:42 -0800

    Create finals.py

---
+++ Intro 2 Programming/finals.py
@@ -0,0 +1,25 @@
+# count 1 to 10
+
+for num in range(1, 11):
+    print (num)
+    
+# Tuple
+
+fruits = ("apple","banana","orange")
+print(tuple[1])
+
+# Bool 
+
+sun_rises = True
+
+if sun_rises:
+    print("Hello World")
+else: 
+    print("Goodbye")
+    
+    
+# Welcome
+def greeting(fname):
+  print("Welcome " + fname)
+
+greeting("Muochu")
