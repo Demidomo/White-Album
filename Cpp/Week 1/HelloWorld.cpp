39389823561ab93f067b6a1e3005a1bf3c53d618
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-03 16:37:31 -0800

    Update HelloWorld.cpp

--- Cpp/Week 1/HelloWorld.cpp
+++ Cpp/Week 1/HelloWorld.cpp
@@ -12,3 +12,27 @@ int main() {
 	printMsg(msg); // call the function
 	return 0;
 }
+
+
+
+// Input full name 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void printMsg(string str) {
+	cout << str;
+}
+
+int main() {
+	cout << "What is your name? ";
+	string firstLastName = "";
+	getline (cin, firstLastName);
+
+	string composedMsg = "Hello " + firstLastName + "! What's up \n";
+	// printMsg(msg); // call the function
+	cout << composedMsg;
+	return 0;
+}
