d356df7be0afde71c7b20f4b007c1325ca06bcfb
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-10 13:04:24 -0800

    Update aone.cpp

--- Cpp/aone.cpp
+++ Cpp/aone.cpp
@@ -1,48 +1,50 @@
 #include <iostream>
+#include <cmath>
 
 void drawShapes() {
-  std::cout << "What shape you like to draw? (Choose 1 for Rectangle and 2 for Right angle triangle)" << std::endl;
+    std::cout << "What shape you like to draw? (Choose 1 for Rectangle and 2 for Right angle triangle)" << std::endl;
 
-  int shape;
-  std::cin >> shape;
+    int shape;
+    std::cin >> shape;
 
-  if (shape == 1) {
-    std::cout << "Enter the width and height of the rectangle: ";
-    int width, height;
-    std::cin >> width >> height;
+    if (shape == 1) {
+        std::cout << "Enter the width and height of the rectangle: ";
+        int width, height;
+        std::cin >> width >> height;
 
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        std::cout << "*";
-      }
-      std::cout << std::endl;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                std::cout << "*";
+            }
+            std::cout << std::endl;
+        }
     }
-  } else if (shape == 2) {
-    std::cout << "Enter the width and height of the triangle: ";
-    int width, height;
-    std::cin >> width >> height;
+    else if (shape == 2) {
+        std::cout << "Enter the width and height of the triangle: ";
+        double width, height;
+        std::cin >> width >> height;
 
-    for (int i = height; i > 0; i--) {
-      for (int j = 0; j < i; j++) {
-        std::cout << "*";
-      }
-      std::cout << std::endl;
+        for (double i = height - 1; i >= 0; i--) {
+            for (double j = 0; j <= round(width * i / height); j++) {
+                std::cout << "*";
+            }
+            std::cout << std::endl;
+        }
     }
-  } else {
-    std::cout << "Invalid input. Please choose 1 or 2." << std::endl;
-  }
+    else {
+        std::cout << "Invalid input. Please choose 1 or 2." << std::endl;
+    }
 
-  std::cout << "---Do you want to try again? (y for yes, n for no)" << std::endl;
-  char tryAgain;
-  std::cin >> tryAgain;
+    std::cout << "---Do you want to try again? (y for yes, n for no)" << std::endl;
+    char tryAgain;
+    std::cin >> tryAgain;
 
-  if (tryAgain == 'y') {
-    drawShapes();
-  }
+    if (tryAgain == 'y') {
+        drawShapes();
+    }
 }
 
 int main() {
-  drawShapes();
-  return 0;
+    drawShapes();
+    return 0;
 }
-
