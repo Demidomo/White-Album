97bfb29990c90ac1cc1b22af91599eba369c4349
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-17 09:37:31 -0800

    Create Java02.java

---
+++ Java/Java02.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String userName = "Matt";
+
+        int age = 22; 
+
+        System.out.println(userName + " is " + age + " years old");
+
+
+        // Implicit typecasting 
+        int num3 = 20;
+        
+        short num4 = (short) num3;
+
+        System.out.println(num4);
+    }
+}
