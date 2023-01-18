3ed1bf319c7248ed2df5ab0b994d0573126ac062
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-17 16:54:55 -0800

    Create balanceTest.cpp

---
+++ Cpp/Week 3/AccountInterest/balanceTest.cpp
@@ -0,0 +1,29 @@
+#include "balance.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	DollarAmount d1(12345);				// 123.45
+	DollarAmount d2(1506);				// 15.06
+
+	cout << "d1 is " << d1.toString() << endl;
+	cout << "d2 is " << d2.toString() << endl;
+
+	// Adding d2 to d1 and print d1's value using toString
+	
+	d1.add(d2);
+	cout << "d2 added to d1 is " << d1.toString() << endl;
+	d1.subtract(d2);
+	cout << "d2 subtract to d1 is " << d1.toString() << endl;
+	
+	DollarAmount d3(1000000);
+	cout << "Initial balance " << d3.toString() << endl;
+	d3.addInterest(2, 100);
+	cout << "After balance 2%" << d3.toString() << endl;
+	d3.addInterest(234, 100*100);
+	cout << "After balance 2.34%" << d3.toString() << endl;
+
+
+	return 0;
+}
