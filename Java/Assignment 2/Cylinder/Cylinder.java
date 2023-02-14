4856ebbc24db151535dc34c01cb2ba1ef8ec4ce4
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-02-13 19:28:16 -0800

    Create Cylinder.java

---
+++ Java/Assignment 2/Cylinder/Cylinder.java
@@ -0,0 +1,20 @@
+package Cylinder;
+import java. lang. Math.*;
+
+public class Cylinder {
+    private double height;
+    private double radius;
+
+    public Cylinder(){
+        
+    }
+  
+    public Cylinder(double height, double radius) {
+        this.height = height;
+        this.radius = radius;
+    }
+  
+    public double getVolume() {
+        return Math.PI * Math.pow(radius, 2) * height;
+    }
+}
