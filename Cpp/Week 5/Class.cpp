5c692c3479d65353b84229b7e72f3b7d19b6991e
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-02-18 17:15:46 -0800

    Create Class.cpp

---
+++ Cpp/Week 5/Class.cpp
@@ -0,0 +1,23 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+
+
+// template for its objects
+class Book {
+    public: 
+        string title;
+        string author;
+        int pages;
+};
+
+
+int main() {
+
+    Book book1; 
+    book1.title = "James Webb";
+    book1.author = "NASA";
+    book1.pages = 2232;
+    
+    return 0;
+}
