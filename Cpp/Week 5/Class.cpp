a7e312a91a94ab54455b4ea596c50a6ecb4c5e43
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-02-18 17:21:42 -0800

    Update Class.cpp

--- Cpp/Week 5/Class.cpp
+++ Cpp/Week 5/Class.cpp
@@ -1,9 +1,9 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
+using namespace std;
 
 
-
-// template for its objects
+// template 
 class Book {
     public: 
         string title;
@@ -18,6 +18,14 @@ int main() {
     book1.title = "James Webb";
     book1.author = "NASA";
     book1.pages = 2232;
+    book1.title = "Whaaaaat";
+    
+    Book book2; 
+    book2.title = "Onetwothree";
+    book2.author = "OTT";
+    book2.pages = 123;
+    
+    cout << book1.title;
     
     return 0;
 }
