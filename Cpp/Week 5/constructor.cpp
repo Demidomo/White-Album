b35fd529841508e15493c32cdf36b63ed4483bcd
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-02-18 17:26:42 -0800

    Create constructor.cpp

---
+++ Cpp/Week 5/constructor.cpp
@@ -0,0 +1,47 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+// template 
+class Book {
+    public: 
+        string title;
+        string author;
+        int pages;
+        
+        // Default constructor
+        Book() {
+            title = "None";
+            author = "No one";
+            pages = 0;
+        }
+        
+        
+        // constructor, created everytime an instance is created 
+        Book(string aTitle, string aAuthor, int aPages) {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+        
+
+
+    
+};
+
+// Object, an instance of the parent class
+int main() {
+
+
+    // values will be passed in to the argument to save lines
+    Book book1("James Webb", "NASA", 2232);
+    book1.title = "Updated title for book 1";
+    Book book2("Onetwothree", "OTT", 123); 
+    Book book3; // Default value that was set earlier will be passed without needing arguments 
+   
+    
+    cout << book3.title;
+    
+    return 0;
+}
