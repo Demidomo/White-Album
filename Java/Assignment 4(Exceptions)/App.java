d97d2b70fdad20893ce496a615844e1e5a23bf3b
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-03-02 15:25:56 -0800

    Update App.java

--- Java/Assignment 4(Exceptions)/App.java
+++ Java/Assignment 4(Exceptions)/App.java
@@ -1,1 +1,20 @@
 import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter number : ");
+            int userNum = sc.nextInt();
+            Number num = new Number(userNum);
+            int result = num.factorial(userNum);
+
+            System.out.println("Factorial returns " + result);
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
