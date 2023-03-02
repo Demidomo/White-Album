b68e150fa8b0c545872792de7b5a495e1e43d164
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-03-02 15:26:28 -0800

    Update Number.java

--- Java/Assignment 4(Exceptions)/Number.java
+++ Java/Assignment 4(Exceptions)/Number.java
@@ -6,3 +6,33 @@
 // 5! --> 5 * 4 * 3 * 2 * 1
 // 3! --> 3 * 2 * 1
 // 6! --> 6 * 5 * 4 * 3 * 2 * 1
+
+
+public class Number {
+   private int number;
+
+    public Number(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int factorial(int num) throws NegativeException{
+        if (num < 0){
+            throw new NegativeException("Negative Number is not allowed! Please enter a whole number.");
+
+        }
+        
+        int result = 1;
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
